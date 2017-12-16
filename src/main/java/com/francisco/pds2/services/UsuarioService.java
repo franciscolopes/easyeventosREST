10ae26312fe574c74a91789d01df290cddb398a9
Francisco Lopes <franciscolopesdacunha@gmail.com>
2017-12-16 20:19:07 -0200

    Exception

--- src/main/java/com/francisco/pds2/services/UsuarioService.java
+++ src/main/java/com/francisco/pds2/services/UsuarioService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.francisco.pds2.domain.Usuario;
 import com.francisco.pds2.repositories.UsuarioRepository;
+import com.francisco.pds2.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UsuarioService {
@@ -12,6 +13,11 @@ public class UsuarioService {
 	private UsuarioRepository usuarioRepo;
 	public Usuario find(Integer codUsuario) {
 		Usuario obj = usuarioRepo.findOne(codUsuario);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! código: " + codUsuario
+					+ ", Tipo: " + Usuario.class.getName());
+		}
+		
 		return obj;
 	}
 	
