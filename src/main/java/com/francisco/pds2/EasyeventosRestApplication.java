c7b1ce6fee16b1428bc49d6e94941a77c5617c75
Francisco Lopes <franciscolopesdacunha@gmail.com>
2017-12-16 18:54:43 -0200

    Operação de instanciação

--- src/main/java/com/francisco/pds2/EasyeventosRestApplication.java
+++ src/main/java/com/francisco/pds2/EasyeventosRestApplication.java
@@ -1,12 +1,29 @@
 package com.francisco.pds2;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.francisco.pds2.domain.Usuario;
+import com.francisco.pds2.repositories.UsuarioRepository;
+
 @SpringBootApplication
-public class EasyeventosRestApplication {
+public class EasyeventosRestApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyeventosRestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Usuario usuario1 = new Usuario(null, "joao", "email", "cpf");
+		Usuario usuario2 = new Usuario(null, "ana", "email", "cpf");
+		usuarioRepository.save(Arrays.asList(usuario1, usuario2));
 	}
 }
