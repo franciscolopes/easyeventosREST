405d6ff8f759b8441e1b6f03c6542be1bad7301e
Francisco Lopes <franciscolopesdacunha@gmail.com>
2017-12-16 18:18:12 -0200

    Banco de dados H2

--- src/main/java/com/francisco/pds2/domain/Usuario.java
+++ src/main/java/com/francisco/pds2/domain/Usuario.java
@@ -2,10 +2,18 @@ package com.francisco.pds2.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
 	private Integer codUsuario;
 	private String nome;
 	private String email;
